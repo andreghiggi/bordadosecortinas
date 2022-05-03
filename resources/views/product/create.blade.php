80682975cad440e6fb9433bdab42427e7b47b829
Mateus Figueredo <mateusfigueredo037@gmail.com>
2022-05-02 22:08:57 -0300

    changes on add products

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -42,59 +42,68 @@
         <div class="container">
             <div class="main-container">
                 <div class="container">
-                    <h2>Cadastrar Produto</h2>
-                    <div class="table">
-                        <form class="table" method="POST" action="" autocomplete="off">
+                    <div class="container">
+                        <h2>Cadastrar Produto</h2>
+                        <form method="POST" action="" autocomplete="off">
                             @csrf
-                            <div class="form">
-                                <div class="col">
-                                    <label class="form-label" for="nome">Referencia:</label><br/>
-                                    <input class="form-control" class="field" type="text" name="referencia" id="referencia"/>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-sm-2">
+                                        <label class="form-label" for="nome">Referencia:</label><br/>
+                                        <input class="form-control" class="field" type="text" name="referencia" id="referencia"/>
+                                    </div>
                                 </div>
-                                <div class="col">
-                                    <label class="form-label" for="contacts">Nome do Produto</label><br/>
-                                    <input class="form-control" type="text" name="produto" id="produto"/>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <label class="form-label" for="contacts">Nome do Produto</label><br/>
+                                        <input class="form-control" type="text" name="produto" id="produto"/>
+                                    </div>
                                 </div>
-                                <div class="col">
-                                    <label class="form-label" for="contacts">Valor</label><br/>
-                                    <input class="form-control" type="text" name="valor" id="valor"/>
+                                <div class="row">
+                                    <div class="col-sm-2">
+                                        <label class="form-label" for="contacts">Valor</label><br/>
+                                        <input class="form-control" type="text" name="valor" id="valor"/>
+                                    </div>
                                 </div>
-                                <div class="col">
-                                    <input class="btn btn-primary" type="submit" value="Salvar"></input>
-                                    <a type="button" class="btn btn-warning"  href="{{ route('main') }}">Voltar</a>
+                                <div class="row">
+                                    <div class="col-sm-2" style="margin-top: 1rem">
+                                        <input class="btn btn-primary" type="submit" value="Salvar"></input>
+                                        <a type="button" class="btn btn-warning"  href="{{ route('main') }}">Voltar</a>
+                                    </div>
                                 </div>
                             </div>
                         </form>
+                        <br>
+                        <div class="container">
+                            <table class="table">
+                                <thead>
+                                  <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Referencia</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Valor</th>
+                                    <th scope="col">Data de criação</th>
+                                    <th scope="col">Ações</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($produto as $item)
+                                  <tr>
+                                    <th scope="row">{{$item->id}}</th>
+                                    <td>{{$item->referencia}}</td>
+                                    <td>{{$item->nome}}</td>
+                                    <td>{{'R$ '.number_format(($item->valor / 100), 2, ',', '.')}}</td>
+                                    <td>{{ $item->created_at->format('d-m-Y')}}</td>
+                                    <td><div>
+                                        <a type="button" class="btn btn-danger"
+                                            href="{{ route('product.delete', ['id' => $item->id]) }}">Deletar</a>
+                                    </div></td>
+                                  </tr>
+                                  @endforeach
+                                </tbody>
+                              </table>
+                        </div>
                     </div>
-                </div>
-                <div class="container">
-                    <table class="table">
-                        <thead>
-                          <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Referencia</th>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Valor</th>
-                            <th scope="col">Data de criação</th>
-                            <th scope="col">Ações</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($produto as $item)
-                          <tr>
-                            <th scope="row">{{$item->id}}</th>
-                            <td>{{$item->referencia}}</td>
-                            <td>{{$item->nome}}</td>
-                            <td>{{'R$ '.number_format(($item->valor / 100), 2, ',', '.')}}</td>
-                            <td>{{ $item->created_at->format('d-m-Y')}}</td>
-                            <td><div>
-                                <a type="button" class="btn btn-danger"
-                                    href="{{ route('product.delete', ['id' => $item->id]) }}">Deletar</a>
-                            </div></td>
-                          </tr>
-                          @endforeach
-                        </tbody>
-                      </table>
                 </div>
             </div>
         </div>
